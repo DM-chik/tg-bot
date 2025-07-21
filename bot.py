c273dbbaf1869b50db886b2cd2a490dad98b86c6
DM-chik <mardmal07@mail.ru>
2025-07-21 17:13:37 +0600

    Hello World1

--- bot.py
+++ bot.py
@@ -1,6 +1,17 @@
-import aiogram
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import Command
+from aiogram.types import Message
+import asyncio
 from dotenv import load_dotenv
 import os
+
+
+
 load_dotenv()
 bot_token = os.getenv('TOKEN')
-print(bot_token)
+dp = Dispatcher()
+
+
+@dp.message(Command('start'))
+async def start(message: Message):
+    await message.answer('Привет')
